1ac87de18546694c8a69f1d97810cce7237eaa76
Arun Seehra <seehra.a@gmail.com>
2018-11-09 10:52:35 -0600

    Implement ADD_ITEM action

--- crud-reducer/src/reducers/index.js
+++ crud-reducer/src/reducers/index.js
@@ -7,6 +7,15 @@ const initialState = {
 export const crudReducer = (state=initialState, action) => {
     // Add code which handles each action here
     if (action.type === ADD_ITEM) {
+        // Create a new array with the action's item appened to state.items
+        const newItems = [...state.items, action.item];
+        // Overwrite the items array in state with our new items array
+        const newState =  {
+            ...state,
+            items: newItems,
+        };
+
+        return newState;
     }
     else if (action.type === UPDATE_ITEM) {
     }
